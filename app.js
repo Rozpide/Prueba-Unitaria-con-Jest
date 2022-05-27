58a215ac7b7dc6359982bf24ce49baa981b043e1
Rozpi <joserozpide@yahoo.es>
2022-05-27 09:46:46 +0000

    cambio-jest270522

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+// esta es mi función que suma dos números
+// const sum = (a,b) => {
+//     return a + b
+// }
+
+// solo un registro en consola para nosotros.
+// console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+// module.exports = { sum };
 //  const sum = (a,b)=>{
 //       return a+b
 //   }
 //   console.log(sum(7,3));
 
 //   module.exports={sum};
-// const fromEuroToDollar=(a,b)=>{
-//  return a*b
+
+ const fromEuroToDollar=(a,b)=>{
+  return a*b
     
-// };
-// console.log(fromEuroToDollar(1,1.2))
-
-// module.exports={fromEuroToDollar};
-
-const fromDollarToYen=(a,b)=>{
-    return a*b
 };
-console.log(fromDollarToYen(1,106.157))
-module.exports={fromDollarToYen};
+ console.log(fromEuroToDollar(1,1.2))
 
-// const fromYanToPound=(a,b)=>{
-//     return a*b
-// };
-// console.log(fromYanToPound(1,106.157))
-// module.exports={fromYanToPound};
+ module.exports={fromEuroToDollar};
+
+ const fromDollarToYen=(a,b)=>{
+     return a*b
+ };
+ console.log(fromDollarToYen(1,102.3))
+ module.exports={fromDollarToYen};
+
+ const fromYanToPound=(a,b)=>{
+     return a*b
+ };
+ console.log(fromYanToPound(1,0.0063))
+ module.exports={fromYanToPound};
 
--- test.js
+++ test.js
@@ -3,24 +3,47 @@
 //     let total = sum(14, 9);
 //     expect(total).toBe(23);
 // });
-// const { fromEuroToDollar } = require('./app.js');      
-// test("One euro should be 1.206 dollars", function(){
+ const { fromEuroToDollar } = require('./app.js');      
+//
+
+test("One euro should be 1.2 dollars", function (){
 //     // importo la funcion desde app.js
-   
-//    let total= fromEuroToDollar(3.5,1.2)
+const fromEuroToDollar=(a,b)=>{
+    return a*b
+      
+  };
+   console.log(fromEuroToDollar(1,1.2))
+  
+   module.exports={fromEuroToDollar};
+//
+    let total= fromEuroToDollar(3.5,1.2);
     
     
 //  // hago mi comparacion (la prueba)
-//     expect(total).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
-// });
+
+     expect(total).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+ });
 const {fromDollarToYen}= require("./app.js");
 test("One dollar should be 106.157 yen", function(){
-    let totaly= fromDollarToYen(5,106.157)
-    expect(totaly).toBe(530.785)
+
+    const fromDollarToYen=(a,b)=>{
+        return a*b
+    };
+    console.log(fromDollarToYen(1,102.3))
+    module.exports={fromDollarToYen};
+
+    let totaly= fromDollarToYen(5,102.3)
+    expect(totaly).toBe(511.5)
 })
 
-// const {fromYanToPound}= require("./app.js");
-// test("One dollar should be 106.157 yen", function(){
-//     let totalyr= fromYanToPound(5,0.0063)
-//     expect(totalyr).toBe(0.0315)
-// })
+ const {fromYanToPound}= require("./app.js");
+ test("One dollar should be 106.157 yen", function(){
+
+    const fromYanToPound=(a,b)=>{
+        return a*b
+    };
+    console.log(fromYanToPound(1,0.0063))
+    module.exports={fromYanToPound};
+     let totalyr= fromYanToPound(5,0.0063)
+     expect(totalyr).toBe(0.0315)
+ })
